85508c8462157fa82018fba7ad7db68b9297b47c
Sebastian Büge <buege@subshell.com>
2023-07-29 22:12:32 +0200

    update board interface

--- app/src/main/java/tictactoe/sebastian/game/Board.java
+++ app/src/main/java/tictactoe/sebastian/game/Board.java
@@ -1,10 +1,14 @@
 package tictactoe.sebastian.game;
 
+import java.util.List;
+
 public interface Board {
 
     void init();
     
     boolean existsFreeCell();
+
+    List<Cell> getFreeCells();
 
     void setCellValue(int xPosition, int yPosition, CellValue value);
 
